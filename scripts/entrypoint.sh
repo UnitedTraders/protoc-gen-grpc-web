03bc2d79880039189346f604b0c26f078cb08009
Alik Kurdyukov <alik@kurdyukov.com>
2023-10-09 19:20:03 +0400

    fix for proto search

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,11 +6,11 @@ OUTPUT_DIR=/out
 args_with_output_dir=""
 
 for arg in "$@"; do
-    echo "arg: ${arg}"
     args_with_output_dir="${arg}:${OUTPUT_DIR} ${args_with_output_dir}"
 done
+protos_found=$(find ${PROTOS_DIR} -name "*.proto")
 
 exec protoc \
     --proto_path=/usr/local/include:${PROTOS_DIR} \
     ${args_with_output_dir} \
-    ${PROTOS_DIR}/**/*.proto ${PROTOS_DIR}/*.proto
+    ${protos_found}
