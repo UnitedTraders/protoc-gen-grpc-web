29ff47e783387327c14ed7afee3e5f4fcbbf8119
Alik Kurdyukov <alik@kurdyukov.com>
2023-10-13 10:53:21 +0400

    fix for multi-file project

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,9 +8,8 @@ args_with_output_dir=""
 for arg in "$@"; do
     args_with_output_dir="${arg}:${OUTPUT_DIR} ${args_with_output_dir}"
 done
-protos_found=$(find ${PROTOS_DIR} -name "*.proto")
 
 exec protoc \
     --proto_path=/usr/local/include:${PROTOS_DIR} \
     ${args_with_output_dir} \
-    ${protos_found}
+    '/protos/**/*.proto'
